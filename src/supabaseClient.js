8a0b96d0feb561fba08a8bf378834785ddbb0565
SiVenn92 <vendrisetyawan9@gmail.com>
2026-07-11 04:52:51 +0700

    Update konfigurasi dan fix nama gambar

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,9 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-// 1. Copy Project URL Anda dari gambar tersebut dan paste di sini:
-const supabaseUrl = 'https://buvpjozmcuehsrtwhehb.supabase.co' 
-
-// 2. Copy tulisan anon key (kunci panjang) dari gambar tersebut dan paste di sini:
-const supabaseKey = 'sb_publishable_BWNLX_M5Sjl7Ie4bfHfkjA_JvXvIA6z' 
+const supabaseUrl = 'https://buvpjozmcuehsrtwhehb.supabase.co'
+const supabaseKey = 'sb_publishable_BWNLX_M5Sj... (dan seterusnya)'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
