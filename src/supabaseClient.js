3f3e39aa1e508ecb7e2aa2a2753d228872a34eb3
SiVenn92 <vendrisetyawan9@gmail.com>
2026-07-11 07:45:07 +0700

    fix: use correct anon public key

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://buvpjozmcuehsrtwhehb.supabase.co'
-const supabaseKey = 'sb_publishable_BWNLX_M5Sj... (dan seterusnya)'
+const supabaseKey = 'seyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ1dnBqb3ptY3VlaHNydHdoZWhiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM2NTY5OTgsImV4cCI6MjA5OTIzMjk5OH0.z6krP3FQ69HNxuFIXty69gUgwobj9vFGfUntKz-Rn2U'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
